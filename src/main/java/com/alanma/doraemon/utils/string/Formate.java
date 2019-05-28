589559be373f8cab5bbcb4a7eb3b04e39ecc7763
AlanMa <alanxiaowei@hotmail.com>
2019-05-28 15:48:08 +0800

    reborn

--- src/main/java/com/alanma/doraemon/utils/string/Formate.java
+++
@@ -1,8 +0,0 @@
-package com.alanma.doraemon.utils.string;
-
-public class Formate {
-	public static void main(String[] args) {
-		//固定位数补零
-		System.out.println(String.format("%04d", 1));
-	}
-}
--- src/test/java/com/alanma/doraemon/TestGitCommand.java
+++
@@ -1,5 +0,0 @@
-package com.alanma.doraemon;
-
-public class TestGitCommand {
-
-}
